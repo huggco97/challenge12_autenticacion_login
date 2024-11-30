41e075ae027eb544288673fe1b79dbe0ed21d00f
Hugo Colman <hugocolman997@gmail.com>
2024-11-30 20:06:19 -0300

    feat:se creo ruta admin-user, y se agg funcionalidades js para obtener los datos en la vista admin

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { verifyToken, verifyRole } = require('../middleware/authMiddleware');
+const { pool } = require('../config');
 const router = express.Router();
 
 // Ruta para usuarios regulares
@@ -11,6 +12,19 @@ router.get('/user', verifyToken, verifyRole('Usuario'), (req, res) => {
 // Ruta para administradores
 router.get('/admin', verifyToken, verifyRole('Administrador'), (req, res) => {
     res.sendFile(path.join(__dirname, '../views/admin.html'));
+
+});
+
+router.get('/admin-users', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT id, email, role FROM users');
+        const users = result.rows;
+        console.log(users)
+        res.json(users);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error al obtener los usuarios');
+    }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,58 +1,4 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const cookieParser = require('cookie-parser');
-// const session = require('express-session');
-// const csrf = require('csurf');
-// const helmet = require('helmet');
-// const path = require('path');
-// const { connectDB } = require('./config');
-// const { initializeUserTable } = require('./models/User');
 
-// const app = express();
-// const csrfProtection = csrf({ cookie: true });
-
-// // Middleware
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(cookieParser());
-// app.use(session({
-//     secret: 'secretKey',
-//     resave: false,
-//     saveUninitialized: true,
-//     cookie: { secure: false, httpOnly: true },
-// }));
-// app.use(helmet());
-// app.use(express.static(path.join(__dirname, 'public')));// Configuracion de archivos estaticos
-
-// // Conectar a PostgreSQL
-// connectDB();
-
-// // inicializar la tabla users
-// initializeUserTable();
-
-// // Ruta para la página principal
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'views', 'index.html'));
-// });
-
-// const authRoutes = require('./routes/authRoutes');
-// app.use('/auth', authRoutes);
-
-// const userRoutes = require('./routes/userRoutes');
-// // Rutas para usuarios y administradores
-// app.use('/', userRoutes);
-
-
-// //configuracion CSRF token 
-// app.get('/csrf-token', csrfProtection, (req, res) => {
-//     res.json({ csrfToken: req.csrfToken() });
-// });
-
-// // iniciar server
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//     console.log(`Server running on http://localhost:${PORT}`);
-// });
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -61,11 +7,13 @@ const csrf = require('csurf');
 const helmet = require('helmet');
 const path = require('path');
 const jwt = require('jsonwebtoken');
+const router = require('./routes/userRoutes')
 const { connectDB } = require('./config');
 const { initializeUserTable } = require('./models/User');
-
 const app = express();
 const csrfProtection = csrf({ cookie: true });
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 // Middleware
 app.use(bodyParser.json());
@@ -91,10 +39,8 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-const authRoutes = require('./routes/authRoutes');
 app.use('/auth', authRoutes);
 
-const userRoutes = require('./routes/userRoutes');
 // Rutas para usuarios y administradores
 app.use('/', userRoutes);
 
