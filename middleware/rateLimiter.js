1ec9b0faaef12fcabf56da69ea2eb76a03f09e61
Hugo Colman <hugocolman997@gmail.com>
2024-12-05 21:06:17 -0300

    feat:cambios en mensaje de rateLimit, proyecto finalizado

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -3,7 +3,7 @@ const rateLimit = require('express-rate-limit');
 const loginLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutos
     max: 5, // Limite cada IP a 5 solicitudes de inicio de sesión por ventana
-    message: 'Too many login attempts. Please try again later.',
+    message: 'Demasiados intentos de inicio de sesión. Inténtelo de nuevo más tarde',
     standardHeaders: true, // Información del límite de tasa de retorno en los encabezados `RateLimit-*`
     legacyHeaders: false, // Deshabilite los encabezados `X-RateLimit-*`
 });
